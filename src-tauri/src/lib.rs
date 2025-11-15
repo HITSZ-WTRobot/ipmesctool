aa9b40cff3399f97c34b0bbb73ba4f5ded7ca678
syhanjin <syhanjin666@gmail.com>
2025-11-15 22:08:48 +0800

    feat: Improve serial port handling and motor feedback

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,14 +1,62 @@
-// Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
-#[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
+use crate::invokes::{connect_motor, disconnect_motor, get_motor_state, list_serial_ports, set_motor_feedback, AppState};
+use log::debug;
+use std::sync::Arc;
+use tauri::{Emitter, Manager};
+use tokio::sync::Mutex;
+
+mod serial;
+mod motor;
+mod error;
+mod command;
+mod config_parser;
+mod invokes;
+
+pub fn start_serial_monitor(app: tauri::AppHandle) {
+    tauri::async_runtime::spawn(async move {
+        let mut last_ports: Vec<String> = vec![];
+
+        loop {
+            let current = match list_serial_ports().await {
+                Ok(ports) => ports,
+                Err(e) => {
+                    debug!("Failed to list serial ports: {}", e);
+                    continue;
+                }
+            };
+
+            if current != last_ports {
+                last_ports = current.clone();
+
+                app.emit("serial-port-changed", current)
+                    .unwrap();
+            }
+
+            tokio::time::sleep(std::time::Duration::from_secs(1)).await;
+        }
+    });
 }
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_opener::init())
-        .invoke_handler(tauri::generate_handler![greet])
+        .setup(|app| {
+            let handle = app.handle();
+            let state = AppState {
+                motor: Arc::new(Mutex::new(None)),
+                app: handle.clone(),
+            };
+            start_serial_monitor(handle.clone());
+            app.manage(state);
+            Ok(())
+        })
+        .invoke_handler(tauri::generate_handler![
+             list_serial_ports,
+            connect_motor,
+            disconnect_motor,
+            get_motor_state,
+            set_motor_feedback
+        ])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
