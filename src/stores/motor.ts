5eb0b85cd91a7eea9d3883a43ab2e11d47657617
syhanjin <syhanjin666@gmail.com>
2025-11-23 18:07:54 +0800

    fix: add motor debug run state atom

--- src/stores/motor.ts
+++ src/stores/motor.ts
@@ -5,7 +5,11 @@ export const motorConnectedAtom = atom<boolean>(false);
 
 export type MotorState = "Stop" | "DebugRun" | "Run" | "Test" | "Fault";
 
+export type MotorDebugRunState = "Stop" | "Position" | "Speed";
+
 export const motorStateAtom = atom<MotorState>("Stop");
+
+export const motorDebugRunStateAtom = atom<MotorDebugRunState>("Stop");
 
 export const motorConfigAtom = atom<MotorConfig>();
 
